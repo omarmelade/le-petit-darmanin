21768441e619a846657921aa01505a68cd1fc827
Omar <omarkhial74@gmail.com>
2023-11-22 12:09:42 +0100

    update footer twitter link

--- src/resources/views/footer.blade.php
+++ src/resources/views/footer.blade.php
@@ -2,6 +2,6 @@
 <div id="footer">
     <div id="footer-content">
         <img alt="Petit logo LePetitDarmanin" src="{{ asset('images/ledarmanin-white.png') }}" id="logo-footer-img">
-        <p>2023 <a class="footer-acc-link" href="https://twitter.com/omar_melade">@omar_melade</a></p>
+        <p>2023 <a class="footer-acc-link" href="https://twitter.com/lepetitdarmanin">@lepetitdarmanin</a></p>
     </div>
 </div>
